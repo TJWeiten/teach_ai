075bf3cc384dcbf00e72ed72bae8ce6c340c3ea5
T.J. Weiten <tjweiten@neogenics.net>
2013-05-30 00:01:49 -0700

    Clean Install
    This commit cleans up all the code and provides installation
    instructions. In theory, the platform should be completely portable now
    to any system.

--- core/database/connect.php
+++ core/database/connect.php
@@ -1,6 +1,6 @@
 <?php
 $select_db_error = '<b>Sorry, we\'re experiencing problems selecting the database!</b>';
 $connect_error = '<b>Sorry, we\'re experiencing problems connecting to the database!</b>';
-mysql_connect('localhost', 'root', 'X7TWLv9hSPbEWvtF') or die($connect_error);
-mysql_select_db('ai') or die($select_db_error);
+mysql_connect('localhost', 'username', 'password') or die($connect_error);
+mysql_select_db('database') or die($select_db_error);
 ?>
---
+++ core/database/install.sql
@@ -0,0 +1,55 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.2.2
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 30, 2013 at 08:46 AM
+-- Server version: 5.5.27
+-- PHP Version: 5.4.7
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `ai`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `games`
+--
+
+CREATE TABLE IF NOT EXISTS `games` (
+  `user_id` int(11) NOT NULL,
+  `replay` mediumtext NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(32) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `first_name` varchar(64) NOT NULL,
+  `last_name` varchar(32) NOT NULL,
+  `email` varchar(1024) NOT NULL,
+  `active` int(11) NOT NULL DEFAULT '1',
+  `bot_working` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=26 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
